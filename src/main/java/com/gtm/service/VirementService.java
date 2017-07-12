f401b0df558b8ff4ed1957c07f8de2708707b0e9
Matthieu DUMOULIN <m.dumoulin.ensci@gmail.com>
2017-07-12 22:04:01 +0200

    Travail sur le VirementService et Log4J

--- src/main/java/com/gtm/service/VirementService.java
+++ src/main/java/com/gtm/service/VirementService.java
@@ -1,12 +1,39 @@
 package com.gtm.service;
 
+import org.apache.log4j.Logger;
+
 import com.gtm.domaine.Compte;
+import com.gtm.domaine.CompteCourant;
+import com.gtm.domaine.CompteEpargne;
 import com.gtm.exception.SaisieException;
 
 public class VirementService {
 
-	public boolean EffectuerVirement(Compte compteDebite, Compte compteCredite, double montant) throws SaisieException{
-		if(montant>compteDebite.getSolde())throw SaisieException("Le solde du compte débité n'est pas assez élevé pour effectuer ce virement")
+	private final static Logger log = Logger.getLogger(VirementService.class);
+
+	public boolean EffectuerVirement(CompteCourant compteDebite, Compte compteCredite, long montant) throws SaisieException{
+		if(montant>(compteDebite.getSolde()+compteDebite.getPlafondDeDecouvert()))throw SaisieException("Le solde du compte "
+				+ "débité n'est pas assez élevé pour effectuer ce virement")
+		else{long SoldeCD = compteDebite.getSolde();
+			long SoldeCC = compteCredite.getSolde();
+			compteDebite.setSolde(SoldeCD - montant);
+			compteCredite.setSolde(SoldeCC + montant);
+			log.info("Virement [Numero CompteDébité = " + compteDebite.getIdCompte() + ", Numero CompteCrédité = " + 
+			compteCredite.getIdCompte() + ", Montant = " + montant + ", Nouveau solde du CompteDébité = "
+			+ compteDebite.getSolde() + " €, Nouveau solde du Compte Crédité = " +compteCredite.getSolde() +" € ]" );
+		}
+		
+		public boolean EffectuerVirement(CompteEpargne compteDebite, Compte compteCredite, long montant) throws SaisieException{
+			if(montant>compteDebite.getSolde())throw SaisieException("Le solde du compte "
+					+ "débité n'est pas assez élevé pour effectuer ce virement")
+			else{long SoldeCD = compteDebite.getSolde();
+				long SoldeCC = compteCredite.getSolde();
+				compteDebite.setSolde(SoldeCD - montant);
+				compteCredite.setSolde(SoldeCC + montant);
+				log.info("Virement [Numero CompteDébité = " + compteDebite.getIdCompte() + ", Numero CompteCrédité = " + 
+				compteCredite.getIdCompte() + ", Montant = " + montant + ", Nouveau solde du CompteDébité = "
+				+ compteDebite.getSolde() + " €, Nouveau solde du Compte Crédité = " +compteCredite.getSolde() +" € ]" );
+			}
 		
 	}
 }
