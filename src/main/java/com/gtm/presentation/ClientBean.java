18bd083d0fef776fa21189a5ed8862c80f2a444b
Alex Pic <alexandre.piccini@laposte.net>
2017-07-12 11:48:53 +0200

    Squelette site sans trop de css et premier bean

---
+++ src/main/java/com/gtm/presentation/ClientBean.java
@@ -0,0 +1,49 @@
+package com.gtm.presentation;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.gtm.domaine.Client;
+
+public class ClientBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	// Client domaine
+	private Client client;
+
+	// Getter client
+	public Client getClient() {
+		return client;
+	}
+
+	// Setter client
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	// Crée instanciation client
+	public void ClientBeans() {
+		client = new Client();
+	}
+
+	public List<Client> getListeClient() {
+		System.out.println("Debut de la getListeClient");
+		List<Client> ListeClient = null; // Pas encore de service
+		return ListeClient;
+
+	}
+
+	public void addClient() {
+		System.out.println("addClient bean");
+		// APPEL DU SERVICE en utilisant client en parametre
+	}
+
+	public void removeClient() {
+		System.out.println("RemoveClient bean");
+		// APPEL DU SERVICE en utilisant client en parametre
+	}
+}
