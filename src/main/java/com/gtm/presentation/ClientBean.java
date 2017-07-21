8bf92bba9ada21cc16e99c76c9dabefafcd8d71c
Alex Pic <alexandre.piccini@laposte.net>
2017-07-21 08:59:05 +0200

    Commit merge

--- src/main/java/com/gtm/presentation/ClientBean.java
+++ src/main/java/com/gtm/presentation/ClientBean.java
@@ -8,7 +8,6 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.gtm.domaine.Client;
-import com.gtm.service.ClientService;
 import com.gtm.service.IClientService;
 
 @Named
@@ -18,20 +17,20 @@ public class ClientBean implements Serializable {
 	private Client client;
 	@Inject
 	private IClientService service;
-	
-	public ClientBean(){
-		//client = new Client ();
+
+	public ClientBean() {
+		// client = new Client ();
 		System.out.println("\n\n BLABLA \n\n");
 	}
-	
+
 	@PostConstruct
 	public void init() {
 		client = new Client();
 	}
 
 	public void creerClient() {
-		System.out.println("Affichage client bean" + client);
-		service.ajouter(client);
+			System.out.println("Affichage client bean" + client);
+			service.ajouter(client);
 	}
 
 	public IClientService getService() {
@@ -49,7 +48,5 @@ public class ClientBean implements Serializable {
 	public void setClient(Client client) {
 		this.client = client;
 	}
-	
-	
-}
 
+}
